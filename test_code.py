1a726844e0fd3d7b27ce2ec9684c812d745ae361
Maxvo <maxvo25@gmail.com>
2025-08-14 20:58:50 +0930

    fix

--- test_code.py
+++ test_code.py
@@ -12,10 +12,10 @@ def test_eight_characters():
 def test_max_length():
     assert validate_password("StrongPass123!@ABCde") is True
 
-# password has space
+# no space in password
 def test_password_with_space():
-    assert validate_password("My Pass123!") is True  
+    assert validate_password("My Pass123!") is False  
 
 # no special character
 def test_no_special_char():
-    assert validate_password("Password1234") is True 
+    assert validate_password("Password1234") is False 
